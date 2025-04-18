72fab478204f8313c1aca3e523f3de31e59080df
Daniel Holloway <daniel@vendettistudios.com>
2025-04-18 01:01:21 -0700

    fix suspense error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import ClientApp from "../components/ClientApp/clientApp";
 import { projects } from "../components/Projects/utils/data";
+import { Suspense } from "react";
 
 export default function Page() {
   return (
+    <Suspense fallback={null}>
     <div className="scroll-lock">
       <ClientApp projects={projects} />
     </div>
+    </Suspense>
   );
 }
